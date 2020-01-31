744e1433be0985b54b3562a42e7604f40a16677d
Franco Escobar <franco@moni.com.ar>
2020-01-31 16:02:28 -0300

    removed yml settings. Removed sentry as default dependency. Added port to cookiecutter template

--- {{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/settings.py
@@ -1,20 +1,10 @@
 import pathlib
 
-import yaml
 from environs import Env
 
 BASE_DIR = pathlib.Path(__file__).parent
-config_path = BASE_DIR / 'config' / 'settings.yml'
 
 env = Env()
 env.read_env()
 
 SENTRY_DSN = env("SENTRY_DSN")
-
-
-def get_config(path):
-    with open(path) as f:
-        config = yaml.safe_load(f)
-    return config
-
-config = get_config(config_path)
